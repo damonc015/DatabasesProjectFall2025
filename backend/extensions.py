dcb6c6576381ab38657dacbb7679b08b100da72d
syliloquy <syliloquy@syliloquys-mbp.mynetworksettings.com>
2025-11-04 22:05:37 -0500

    Remove files from cookie cutter flask

---
+++ backend/extensions.py
@@ -0,0 +1,20 @@
+"""Flask extensions"""
+from flask_bcrypt import Bcrypt
+import mysql.connector
+from flask import current_app
+
+bcrypt = Bcrypt()
+
+
+def get_db():
+    """Get database connection - simple version"""
+    conn = mysql.connector.connect(
+        host=current_app.config['MYSQL_HOST'],
+        port=current_app.config['MYSQL_PORT'],
+        user=current_app.config['MYSQL_USER'],
+        password=current_app.config.get('MYSQL_PASSWORD') or '',
+        database=current_app.config['MYSQL_DATABASE']
+    )
+    return conn
+
+
