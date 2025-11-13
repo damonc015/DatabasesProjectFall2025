254f622dbf0d93b4f0864a79d7e5ef257be7dc1c
YukeTian <yuketian@mac.mynetworksettings.com>
2025-11-13 18:36:15 -0500

    Add login, register, welcome guide, household management, and settings features

--- backend/extensions.py
+++ backend/extensions.py
@@ -11,8 +11,7 @@ def get_db():
         port=current_app.config['MYSQL_PORT'],
         user=current_app.config['MYSQL_USER'],
         password=current_app.config.get('MYSQL_PASSWORD') or '',
-        database=current_app.config['MYSQL_DATABASE'],
-        unix_socket=current_app.config.get('MYSQL_UNIX_SOCKET')
+        database=current_app.config['MYSQL_DATABASE']
     )
     return conn
 
