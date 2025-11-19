7b9f8a8739f4f923fec66aedd2ac7d49e74769a5
Damon Chu <dc3800@nyu.edu>
2025-11-18 22:00:21 -0500

    teststates

--- frontend/src/stores/useShoppingListStore.js
+++ frontend/src/stores/useShoppingListStore.js
@@ -1,8 +1,9 @@
 import { create } from 'zustand';
 
 const useShoppingListStore = create((set) => ({
-  isModalOpen: false,
-  isListHistory: 'createlist',
+  isModalOpen: true,
+  // history, createlist, modifylist
+  isListHistory: 'history',
   openModal: () => set({ isModalOpen: true }),
   closeModal: () => set({ isModalOpen: false }),
   toggleModal: () => set((state) => ({ isModalOpen: !state.isModalOpen })),
